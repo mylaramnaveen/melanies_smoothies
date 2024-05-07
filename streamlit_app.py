3fc6ee9b422c9148bcf2fc7423c9f53f49359be4
mylaramnaveen <mylaramnaveen2001@gmail.com>
2024-05-07 12:09:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+check for any erroes and solve it:
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -5,22 +7,23 @@ import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom smoothie!"""
+    """Choose the fruits you want in your custom smoothie!
+    """
 )
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Snowflake connection - Remove if not needed
-# cnx = st.connection("snowflake")
-# session = cnx.session()
-
-# Assuming my_dataframe is retrieved correctly from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:", my_dataframe, max_selections=5)
+    "Choose up to 5 ingredients:"
+    ,my_dataframe
+    ,max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
@@ -28,16 +31,17 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
-
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
-
+    #st.stop()
+    
+    #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
      
     if time_to_insert:
@@ -45,3 +49,4 @@ if ingredients_list:
     
         success_message = f'Your Smoothie is ordered, {name_on_order}!'
         st.success(success_message, icon="✅")
+        
