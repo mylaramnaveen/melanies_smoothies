ad2447a254c8ba3be4b1ff97431b34b14375cc0d
mylaramnaveen <mylaramnaveen2001@gmail.com>
2024-05-07 11:55:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,11 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
-    if fruityvice_response.status_code == 200:
-        fv_data = fruityvice_response.json()
-        # Print the nutritional information
-        st.write(fv_data)
-    else:
-        st.error(f"Failed to fetch nutritional information for {fruit_chosen}. Status code: {fruityvice_response.status_code}")
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    st.write(ingredients_string)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
