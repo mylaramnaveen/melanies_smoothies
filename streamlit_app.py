07bb9d8dc342d73db2e6818ce3b2e1a137396998
mylaramnaveen <mylaramnaveen2001@gmail.com>
2024-05-09 11:05:12 +0530

    Update streamlit_app.py
    A4DA747

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,31 @@
+#check for any erroes and solve it:
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-  
 st.write(
     """Choose the fruits you want in your custom smoothie!
     """
 )
+
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC Function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     ,my_dataframe
@@ -26,12 +36,14 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
     st.write(my_insert_stmt)
     #st.stop()
     
@@ -43,3 +55,4 @@ if ingredients_list:
     
         success_message = f'Your Smoothie is ordered, {name_on_order}!'
         st.success(success_message, icon="✅")
+        
