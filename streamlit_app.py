09f73cb52ce9cd52062d5b8401b5734bac40f589
mylaramnaveen <mylaramnaveen2001@gmail.com>
2024-05-09 11:01:21 +0530

    Update streamlit_app.py
    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,66 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+
+    
+        
+          
+    
+
+        
+        Expand All
+    
+    @@ -23,11 +24,12 @@
+  
 st.write(
     """Choose the fruits you want in your custom smoothie!
     """
 )
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
-
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     ,my_dataframe
     ,max_selections=5)
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+
+    
+        
+          
+    
+
+        
+        Expand All
+    
+    @@ -45,9 +47,5 @@
+  
         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-
-
     st.write(my_insert_stmt)
     #st.stop()
-
+    
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
-
-
+     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-
+    
         success_message = f'Your Smoothie is ordered, {name_on_order}!'
         st.success(success_message, icon="✅")
